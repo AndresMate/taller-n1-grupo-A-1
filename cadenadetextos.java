e8d1437f3300b5549ea6018f62b0f2eabc2e4f2a
AndresMate <andres.mateus01@uptc.edu.co>
2022-12-16 10:29:22 -0500

    cambio de paqueteria

--- cadenadetextos.java
+++ cadenadetextos.java
@@ -1,8 +1,8 @@
-package taller;
+package taller-n1-grupo-A;
 
 public class cadenadetextos {
     public static void main(String[] args) {
-        String cadena = " Hola  mundo";
+        String cadena = " Hola   mundo";
         String cadenaSinEspacios = cadena.replaceAll("\\s+", " ");
         cadenaSinEspacios=cadenaSinEspacios.trim();
         System.out.println(cadenaSinEspacios);
