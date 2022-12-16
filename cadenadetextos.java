16c9dc1af23c15dc88a33cf539cdf7f9029ed5f3
AndresMate <andres.mateus01@uptc.edu.co>
2022-12-16 10:51:25 -0500

    Update cadenadetextos.java
    pruebas

--- cadenadetextos.java
+++ cadenadetextos.java
@@ -2,7 +2,7 @@ package taller_n1_grupo_A;
 
 public class cadenadetextos {
     public static void main(String[] args) {
-        String cadena = " Hola   mundo";
+        String cadena = "  Hola   mundo  ";
         String cadenaSinEspacios = cadena.replaceAll("\\s+", " ");
         cadenaSinEspacios=cadenaSinEspacios.trim();
         System.out.println(cadenaSinEspacios);
